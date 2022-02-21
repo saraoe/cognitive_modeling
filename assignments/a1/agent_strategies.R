7648af06a5d5f111b0b483a4e44fdcdb5c6c2b2b
SaraMøllerØstergaard#1413 <sara.m.oestergaard@gmail.com>
2022-02-21 10:57:58 +0100

    memory agent

--- assignments/a1/agent_strategies.R
+++ assignments/a1/agent_strategies.R
@@ -15,9 +15,9 @@ RandomAgent <- function(bias, noise){
 
 # win-stay-lose-shift
 WSLSAgent <- function(prevChoice, feedback, noise){ 
-  if(Feedback == 1){  # win
+  if(feedback == 1){  # win
     choice <- prevChoice
-  } else if (Feedback == 0) {  # lose
+  } else if (feedback == 0) {  # lose
       choice <- 1 - prevChoice 
   }
   
@@ -45,14 +45,31 @@ WSLSAgent <- function(prevChoice, feedback, noise){
 #   return(choice)
 # }
 
-MemoryAgent <- function(othersChoice, memory, N, noise){
-  memory <- memory+othersChoice
+# MemoryAgent <- function(othersChoice, memory, N, noise){
+#   memory <- memory+othersChoice
+#   
+#   choice <- rbinom(1,1,memory/N)
+#   
+#   if(rbinom(1, 1, noise)==1){  # noise
+#     choice <- rbinom(1,1,0.5)
+#   }
+#   
+#   return(c(choice, memory))
+
+MemoryAgent <- function(memory, mem_contraint, noise){
+  if (mem_contraint < length(memory)){
+    index <-length(memory) - mem_contraint
+    memory <- memory[index:length(memory)]
+  }
+  prob1 <- mean(memory)
   
-  choice <- rbinom(1,1,memory/N)
-  
-  if(rbinom(1, 1, noise)==1){  # noise
-    choice <- rbinom(1,1,0.5)
+  if (prob1 > 0.5){
+    choice <- RandomAgent(1, noise)
+  } else if (prob1 < 0.5) {
+    choice <- RandomAgent(0, noise)
+  } else {
+    choice <- RandomAgent(0.5, noise)
   }
   
-  return(c(choice, memory))
+  return(choice)
 }
